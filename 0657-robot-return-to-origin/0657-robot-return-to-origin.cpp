7c5c341fef3cf9c7a6f6a5b0447a07595a5ed443
Sravanipunnam <147476006+Sravanipunnam@users.noreply.github.com>
2025-12-02 15:35:28 +0530

    Time: 7 ms (11.73%), Space: 10.2 MB (74.35%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int left=0,right=0,up=0,down=0;
+    for(int i=0;i<moves.size();i++)
+     {
+         if(moves[i]=='U'){
+        up++;
+       }
+        else  if(moves[i]=='D'){
+        down++;
+       }
+        else  if(moves[i]=='L'){
+            left++;
+       }else{
+        right++;
+       }
+
+     }
+     if(up==down && left==right){
+        return true;
+     }
+     return false;
+    }
+};
